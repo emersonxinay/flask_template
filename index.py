575cec50f886660ff4a154ef35cb147d2d741857
Emerson Espinoza <xinayespinoza@gmail.com>
2023-02-01 03:51:37 -0300

    diccionarios

--- index.py
+++ index.py
@@ -15,5 +15,14 @@ def login():
   
   return render_template('login.html', name = nombre, age = edad)
 
+
+# diccionarios 
+@app.route('/diccionario')
+def diccionario():
+  dic = {"nombre":"emerson", "edad":"20", "ciudad":"Santiago"}
+  
+  return render_template('login.html', diccionario = dic)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
